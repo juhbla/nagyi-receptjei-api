dd0f61be4846fc4bd509e1248939669ba4d148c8
Adél Juhasz <sz0juhade@vasvari.org>
2023-10-15 21:18:32 +0200

    Recept repo regisztrációja a service container-be

--- NagyiReceptjei.API/Program.cs
+++ NagyiReceptjei.API/Program.cs
@@ -8,6 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseMySQL(builder.Configuration.GetConnectionString("Local")));
 
+builder.Services.AddScoped<RecipeRepository>();
+
 builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
