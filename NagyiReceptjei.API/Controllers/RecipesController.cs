1f9391c99420692e24fa20e8d769033740eb0542
Adél Juhasz <sz0juhade@vasvari.org>
2023-10-15 21:30:33 +0200

    Alap endpointok definiálása.

---
+++ NagyiReceptjei.API/Controllers/RecipesController.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+using NagyiReceptjei.API.Models;
+using NagyiReceptjei.API.Repositories;
+
+namespace NagyiReceptjei.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]/")]
+public class RecipesController
+{
+    private readonly RecipeRepository _recipeRepository;
+
+    public RecipesController(RecipeRepository recipeRepository)
+    {
+        _recipeRepository = recipeRepository;
+    }
+
+    // GET: api/recipes
+    [HttpGet]
+    public IResult GetRecipes()
+    {
+        return null;
+    }
+
+    // POST: api/recipes
+    [HttpPost]
+    public IResult CreateRecipe([FromBody] Recipe request)
+    {
+        return null;
+    }
+
+    // PUT: api/recipes/id
+    [HttpPut("{id:int}")]
+    public IResult UpdateRecipe(int id, [FromBody] Recipe request)
+    {
+        return null;
+    }
+
+    // DELETE: api/recipes/1
+    [HttpDelete("{id:int}")]
+    public IResult DeleteRecipe(int id)
+    {
+        return null;
+    }
+}
