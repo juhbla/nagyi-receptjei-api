83d74a852506aeded8379b5a24e2d0ed8bebd591
Adél Juhasz <sz0juhade@vasvari.org>
2023-11-22 19:48:09 +0100

    Email formátum validáció.

--- NagyiReceptjei.API/Resources/Requests/CreateUserRequest.cs
+++ NagyiReceptjei.API/Resources/Requests/CreateUserRequest.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace NagyiReceptjei.API.Resources.Requests;
 
 public class CreateUserRequest
 {
+    [EmailAddress]
     public string Email { get; set; }
     public string Username { get; set; }
     public string Password { get; set; }
