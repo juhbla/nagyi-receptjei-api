c43d74311b038c4a3c20912ddb1ea27489bdd0d0
juhbla <juhaszblanka25@gmail.com>
2023-11-28 21:00:27 +0100

    Aktuális idő alapértelmezett érték

--- NagyiReceptjei.API/Models/Comment.cs
+++ NagyiReceptjei.API/Models/Comment.cs
@@ -8,5 +8,5 @@ public class Comment
     public int UserId { get; set; }
     public User User { get; set; }
     public string Content { get; set; }
-    public DateTime CreatedDateTime { get; set; }
+    public DateTime CreatedDateTime { get; set; } = DateTime.Now;
 }
